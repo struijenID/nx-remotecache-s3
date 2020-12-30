25621084da27abf0396cc0f99975809675c23b7c
Thiago Barbato <thiago.barbato@rz2.com.br>
2020-12-30 17:44:35 -0300

    change to S3 backed storage

--- index.ts
+++ index.ts
@@ -1,41 +1,81 @@
 import defaultTaskRunner from '@nrwl/workspace/tasks-runners/default';
-import { Storage } from '@google-cloud/storage';
+import { S3 } from '@aws-sdk/client-s3';
 import { join, dirname, relative } from 'path';
-import { promises } from 'fs';
+import { promises, readFileSync, writeFileSync } from 'fs';
 import mkdirp from 'mkdirp';
+import { default as getStream } from 'get-stream'
 
 export default function runner(
     tasks: Parameters<typeof defaultTaskRunner>[0],
-    options: Parameters<typeof defaultTaskRunner>[1] & { bucket?: string },
+    options: Parameters<typeof defaultTaskRunner>[1] & { bucket?: string, key: string, secret: string},
     context: Parameters<typeof defaultTaskRunner>[2],
 ) {
     if (!options.bucket) {
         throw new Error('missing bucket property in runner options. Please update nx.json');
     }
-    const bucket = new Storage().bucket(options.bucket);
+
+    const s3 = new S3({
+        region: 'us-east-1',
+        credentials: {
+            accessKeyId: options.key,
+            secretAccessKey: options.secret
+        }
+    });
+
     return defaultTaskRunner(tasks, { ...options, remoteCache: { retrieve, store } }, context);
 
     async function retrieve(hash: string, cacheDirectory: string): Promise<boolean> {
         try {
-            const commitFile = bucket.file(`${hash}.commit`);
-            if (!(await commitFile.exists())[0]) {
-                return false;
+            const commitFile = `${hash}.commit`;
+            try {
+                await s3.headObject({
+                    Bucket: options.bucket,
+                    Key: `${hash}.commit`,
+                });
+            } catch (e) {
+                if (e.name === 'NotFound') {
+                    return false;
+                } else {
+                    throw e;
+                }
             }
-            const [files] = await bucket.getFiles({ prefix: `${hash}/` });
-            await Promise.all(files.map(download));
+
+            const filesOutput = await s3.listObjects({
+                Bucket: options.bucket,
+                Prefix: `${hash}/`
+            });
+
+            const files = filesOutput.Contents?.map(f => f.Key) || [];
+
+            await Promise.all(files.map(f => {
+                if (f) {
+                    download(f);
+                }
+            }));
             await download(commitFile); // commit file after we're sure all content is downloaded
-            console.log(`retrieved ${files.length + 1} files from cache gs://${bucket.name}/${hash}`);
+            console.log(`retrieved ${files.length + 1} files from cache s3://${options.bucket}/${hash}`);
             return true;
         } catch (e) {
             console.log(e);
-            console.log(`WARNING: failed to download cache from ${bucket.name}: ${e.message}`);
+            console.log(`WARNING: failed to download cache from ${options.bucket}: ${e.message}`);
             return false;
         }
 
-        async function download(file: import('@google-cloud/storage').File) {
-            const destination = join(cacheDirectory, file.name);
+        async function download(fileKey: string) {
+            const destination = join(cacheDirectory, fileKey);
             await mkdirp(dirname(destination));
-            await file.download({ destination });
+            
+            const fileOutput = await s3.getObject({
+                Bucket: options.bucket,
+                Key: fileKey
+            });
+
+            const fileStream = fileOutput.Body!;
+            let contentBuffer: Buffer | null = await getStream.buffer(fileStream as any);
+            
+            if (fileOutput.Body) {
+                writeFileSync(destination, contentBuffer);
+            }
         }
     }
 
@@ -44,11 +84,17 @@ export default function runner(
         try {
             await uploadDirectory(join(cacheDirectory, hash));
             await Promise.all(tasks);
-            await bucket.upload(join(cacheDirectory, `${hash}.commit`)); // commit file once we're sure all content is uploaded
-            console.log(`stored ${tasks.length + 1} files in cache gs://${bucket.name}/${hash}`);
+            
+            // commit file once we're sure all content is uploaded
+            await s3.putObject({
+                Bucket: options.bucket,
+                Key: `${hash}.commit`,
+                Body: readFileSync(join(cacheDirectory, `${hash}.commit`))
+            });
+            console.log(`stored ${tasks.length + 1} files in cache s3://${options.bucket}/${hash}`);
             return true;
         } catch (e) {
-            console.log(`WARNING: failed to upload cache to ${bucket.name}: ${e.message}`);
+            console.log(`WARNING: failed to upload cache to ${options.bucket}: ${e.message}`);
             return false;
         }
 
@@ -60,7 +106,11 @@ export default function runner(
                     await uploadDirectory(full);
                 } else if (stats.isFile()) {
                     const destination = relative(cacheDirectory, full);
-                    tasks.push(bucket.upload(full, { destination }));
+                    tasks.push(s3.putObject({
+                        Bucket: options.bucket,
+                        Key: destination,
+                        Body: readFileSync(full)
+                    }));
                 }
             }
         }
